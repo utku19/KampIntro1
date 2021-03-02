dfb36ece70814607975a63a127e17aaa6f1142fc
utku berk gök <utku_19_berk@hotmail.com>
2021-03-02 17:00:10 +0300

    23 Ocak Ders Sonu

---
+++ OOP2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Engin Demiroğ
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 3;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Engin";
+            musteri1.Soyadi = "Demiroğ";
+            musteri1.TcNo = "3213123123123";
+
+            //Kodlama.io
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "2312312312";
+
+
+            //Gerçek Müşteri - Tüzel Müşteri
+            //SOLİD - L Harfini kullanıyoruz burda
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(musteri1);
+            customerManager.Add(musteri2);
+
+
+        }
+    }
+}
---
+++ OOP3/IKrediManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    //interfaceleri birbirinin alternatifi olan 
+    //ama kod içeriği farklı olan durumlar için kullanırız
+    interface IKrediManager
+    {
+        void Hesapla();
+
+        void BiseyYap();
+        
+    }
+}
